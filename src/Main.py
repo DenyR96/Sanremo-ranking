5785bff30c372a1dd2d7e9d8ba2a97465235f268
Denise Russo <x81000632@studium.unict.it>
2022-07-04 16:37:54 +0200

    test(test_music_player.py):add test_pause for music_player

--- src/Main.py
+++ src/Main.py
@@ -2,10 +2,10 @@ from tkinter import *
 import tkinter as tk
 from tkinter import Canvas, Scrollbar, ttk
 import webbrowser
-from RankingFrame import RankingFrame
-from SongTextFrame import SongTextFrame
-from MusicPlayer import MusicPlayer
-from Commons import color_selection_year, font_btn, color_btn, color_text, font_text, rankings_data, color_canvas, songs_text_data, win
+from src.ranking_frame import RankingFrame
+from src.song_text_frame import SongTextFrame
+from src.music_player import MusicPlayer
+import src.common_variables as common_variables
 
 # sanremo's logos image
 logo_sanremo_2020 = PhotoImage(file = "assets/images/logo_sanremo_2020.png")
@@ -20,7 +20,7 @@ class Main(tk.Frame):
         self.year_chosen = tk.StringVar()
         self.radVar = tk.IntVar()
         self.radVar.set(99) #Select a non-existing index value for radVar
-        self.main_frame = tk.Frame(win, bg = color_selection_year[0])
+        self.main_frame = tk.Frame(common_variables.win, bg = common_variables.color_selection_year[0])
         self.logo_label = Label(self.main_frame, anchor = "n")
 
         # create Ranking frame, Song text frame and Music player instances
@@ -36,18 +36,18 @@ class Main(tk.Frame):
         select_year.set("Choose a year:")
         select_year['values'] = (2022, 2021, 2020)
         select_year.bind("<<ComboboxSelected>>", self.show_ranking)
-        select_year.configure(font = font_btn)
+        select_year.configure(font = common_variables.font_btn)
         self.main_frame.pack(expand = "True", fill = "both")
         select_year.place(relheight = 0.05, relwidth = 0.15, relx = 0.175, rely = 0.04)
         
 		# button that displays song's text inside frame
-        self.text_button = tk.Button(self.main_frame, text = "Show text", font = font_btn, fg = color_text, bg = color_btn, command = self.show_text, cursor = "hand2", state = "disabled")
-        self.text_button.config(highlightthickness = 0, activebackground = color_btn, activeforeground = color_text)
+        self.text_button = tk.Button(self.main_frame, text = "Show text", font = common_variables.font_btn, fg = common_variables.color_text, bg = common_variables.color_btn, command = self.show_text, cursor = "hand2", state = "disabled")
+        self.text_button.config(highlightthickness = 0, activebackground = common_variables.color_btn, activeforeground = common_variables.color_text)
         self.text_button.place(relheight = 0.05, relwidth = 0.1, relx = 0.660, rely = 0.15, anchor = "center")
 
         # button that opens song's url into browser window
-        self.video_button = tk.Button(self.main_frame, text = "Go to video", font = font_btn, fg = color_text, bg = color_btn, command = self.show_video, cursor = "hand2", state = "disabled")
-        self.video_button.config(highlightthickness = 0, activebackground = color_btn, activeforeground = color_text)
+        self.video_button = tk.Button(self.main_frame, text = "Go to video", font = common_variables.font_btn, fg = common_variables.color_text, bg = common_variables.color_btn, command = self.show_video, cursor = "hand2", state = "disabled")
+        self.video_button.config(highlightthickness = 0, activebackground = common_variables.color_btn, activeforeground = common_variables.color_text)
         self.video_button.place(relheight = 0.05, relwidth = 0.1, relx = 0.820, rely = 0.15, anchor = "center")
     
     def song_choice(self):     
@@ -66,14 +66,14 @@ class Main(tk.Frame):
 
 		#depending on the chosen year, the bg color of the main frame and the image will change 
         if self.year_chosen == "2022":
-            self.main_frame.configure(bg = color_selection_year[0])
-            self.logo_label.configure(image = logo_sanremo_2022, bg = color_selection_year[0])
+            self.main_frame.configure(bg = common_variables.color_selection_year[0])
+            self.logo_label.configure(image = logo_sanremo_2022, bg = common_variables.color_selection_year[0])
         elif self.year_chosen == "2021":
-            self.main_frame.configure(bg = color_selection_year[1])
-            self.logo_label.configure(image = logo_sanremo_2021, bg = color_selection_year[1])
+            self.main_frame.configure(bg = common_variables.color_selection_year[1])
+            self.logo_label.configure(image = logo_sanremo_2021, bg = common_variables.color_selection_year[1])
         elif self.year_chosen == "2020":
-            self.main_frame.configure(bg = color_selection_year[2])
-            self.logo_label.configure(image = logo_sanremo_2020, bg = color_selection_year[2])
+            self.main_frame.configure(bg = common_variables.color_selection_year[2])
+            self.logo_label.configure(image = logo_sanremo_2020, bg = common_variables.color_selection_year[2])
 
         self.main_frame.pack(expand = "True", fill = "both")
         self.logo_label.pack()
@@ -85,10 +85,10 @@ class Main(tk.Frame):
 
         posizione_classifica = 1
         pos_y = 90
-        for canzone in rankings_data[self.year_chosen]:
+        for canzone in common_variables.rankings_data[self.year_chosen]:
             dati_canzone = str(posizione_classifica) + " \"" + canzone['title'] + "\" " + canzone['singer']
-            self.curRad = tk.Radiobutton(rank_frame, text = dati_canzone, font = font_text, variable = self.radVar, value = posizione_classifica - 1, command = self.song_choice, bg = color_canvas, cursor = "hand2")
-            self.curRad.config(highlightthickness = 0, activebackground = color_canvas, activeforeground = "#46ebfc", selectcolor = color_canvas, fg = color_text)
+            self.curRad = tk.Radiobutton(rank_frame, text = dati_canzone, font = common_variables.font_text, variable = self.radVar, value = posizione_classifica - 1, command = self.song_choice, bg = common_variables.color_canvas, cursor = "hand2")
+            self.curRad.config(highlightthickness = 0, activebackground = common_variables.color_canvas, activeforeground = "#46ebfc", selectcolor = common_variables.color_canvas, fg = common_variables.color_text)
             rank_canvas.create_window(self.ranking_instance.rank_entries_left, pos_y, anchor = 'w', window = self.curRad)
             pos_y = pos_y + 30
             posizione_classifica = posizione_classifica + 1
@@ -100,18 +100,18 @@ class Main(tk.Frame):
         num_canzone_scelta = self.song_choice()
         self.song_text_instance.place(relheight = 0.78, relwidth = 0.47, relx = 0.51, rely = 0.2)
         text_vbar.pack(side = "right", fill = "y")
-        song_title = rankings_data[self.year_chosen][num_canzone_scelta]['title']
+        song_title = common_variables.rankings_data[self.year_chosen][num_canzone_scelta]['title']
         self.song_text_instance.text_title.configure(text = song_title)
         text_canvas.create_window(self.song_text_instance.song_text_frame_center, 20, window = self.song_text_instance.text_title, anchor = "n")
-        canzone = songs_text_data[self.year_chosen][num_canzone_scelta]["song_text"]
+        canzone = common_variables.songs_text_data[self.year_chosen][num_canzone_scelta]["song_text"]
         self.song_text_instance.song_text.configure(text = canzone)
         text_canvas.create_window(self.song_text_instance.song_text_frame_center, 80, window = self.song_text_instance.song_text, anchor = "n")
     
     def show_video(self):
         num_canzone_scelta = self.song_choice()
-        canzoni = rankings_data[self.year_chosen]
+        canzoni = common_variables.rankings_data[self.year_chosen]
         webbrowser.open_new(canzoni[num_canzone_scelta]['url'])
     
 if __name__ == "__main__":
-    main = Main(win)
+    main = Main(common_variables.win)
     main.mainloop()
---
+++ tests/test_music_player.py
@@ -0,0 +1,42 @@
+
+import tkinter
+from pytest_mock import MockerFixture
+from src.music_player import MusicPlayer
+from src.common_variables import win
+from src.main import Main
+import os
+
+
+
+def test_pause(mocker:MockerFixture) -> None:
+    win = tkinter.Tk()
+    win.withdraw()
+    music_player = MusicPlayer(win)
+    assert music_player.playing_state == False
+    music_player.pause()
+    assert music_player.playing_state == True
+    assert music_player.pause_resume.get() == 'Pause'
+    music_player.pause()
+    assert music_player.pause_resume.get() == 'Resume'
+    assert music_player.playing_state == False
+    win.destroy()
+
+"""def test_stop(mocker:MockerFixture) -> None:
+    win = tkinter.Tk()
+    win.withdraw()
+    music_player = MusicPlayer(win)
+    assert music_player.playing_state == False #perchè lo stato di default è a false
+    mock_close_val = None
+    mocker.patch.object(music_player, "os.close", return_value=mock_close_val)
+    spy = mocker.spy(music_player, "os.close")
+    res = music_player.stop()
+    
+    assert music_player.playing_state == False #perchè stop() mette a false lo stato
+    assert music_player.play_button['state'] == 'active'
+    assert music_player.pause_button['state'] == 'disabled'
+    assert music_player.stop_button['state'] == 'disabled'
+    
+    assert res is None
+    assert spy.call_count == 1
+    assert spy.spy_return == mock_close_val
+    win.destroy()"""
