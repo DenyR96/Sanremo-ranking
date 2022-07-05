2fbcd5eb5739f9a700dc5c8baf975717ad5678ec
dami1411 <damifinocchiaro@gmail.com>
2022-07-05 11:28:06 +0200

    test(test_music_player.py): add load_test for music_player

--- tests/test_music_player.py
+++ tests/test_music_player.py
@@ -1,7 +1,35 @@
 import tkinter
 from pytest_mock import MockerFixture
-from src.common_variables import win
+from src.common_variables import win, os_name
 import src.music_player as mp
+import platform
+
+
+
+
+def test_load(mocker:MockerFixture) -> None:
+    mocker.patch.object(mp, "os", return_value = {})
+    spy_open = mocker.spy(mp.os, "open")
+    mocker.patch.object(mp, "os", return_value = {})
+    spy_write = mocker.spy(mp.os, "write")
+    
+    win = tkinter.Tk()
+    win.withdraw()
+    music_player = mp.MusicPlayer(win)
+    music_player.load('2022', 0)
+    mock_song_title = 'Brividi'
+    assert music_player.song_title == mock_song_title 
+    mock_mp3_link = "https://denyr96.github.io/Sanremo-ranking/assets/songs/2022/Brividi.mp3"
+    assert music_player.mp3_link == mock_mp3_link
+    mock_os_name = platform.system()
+    assert os_name == mock_os_name 
+    assert music_player.play_button['state'] == 'active'
+    assert music_player.pause_button['state'] == 'disabled'
+    assert music_player.stop_button['state'] == 'disabled'
+
+    assert spy_open.called_once_with(music_player.file_path)
+    assert spy_write.called_once_with(music_player.mp3_file)
+
 
 def test_play(mocker:MockerFixture) -> None:
     mocker.patch.object(mp, "os", return_value = {})
@@ -53,15 +81,7 @@ def test_stop(mocker:MockerFixture) -> None:
     music_player.mp3_file = mock_audio_file
 
     assert music_player.playing_state == False #perchè lo stato di default è a false
-<<<<<<< HEAD
-    mock_close_val = None
-    mocker.patch.object(music_player, "os", return_value=mock_close_val)
-    spy = mocker.spy(music_player, "os.close")
-    res = music_player.stop()
-    
-=======
     music_player.stop()
->>>>>>> 4ed732bf4d05ed90666e6f497da74325ff21e7e4
     assert music_player.playing_state == False #perchè stop() mette a false lo stato
     assert music_player.play_button['state'] == 'active'
     assert music_player.pause_button['state'] == 'disabled'
@@ -70,3 +90,5 @@ def test_stop(mocker:MockerFixture) -> None:
     win.destroy()
 
     assert spy.called_once_with(mock_audio_file)
+
+    
